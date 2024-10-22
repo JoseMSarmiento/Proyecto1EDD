880538cac1aba2219786e4ac94b754b29fecd224
JM <j.sarmiento@correo.unimet.edu.ve>
2024-10-21 23:20:41 -0400

    Clase NodoLista

---
+++ src/proyectojosemanuel/NodoLista.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package proyectojosemanuel;
+
+/**
+ *
+ * @author 
+ */
+public class NodoLista {
+    Estacion estacion;
+    NodoLista pNext;
+    NodoLista pPrev;
+    
+    public NodoLista(Estacion station){
+        this.estacion = station;
+        this.pNext =this.pPrev =  null;
+    }
+}
